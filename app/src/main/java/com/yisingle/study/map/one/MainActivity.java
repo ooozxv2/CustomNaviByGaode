4a74a3fb5ede9a2cd9a55bf9d9042ed43dba881d
JIKUN <j314815101@qq.com>
2018-04-08 14:58:05 +0800

    添加上传到jcenter的工具方法

--- app/src/main/java/com/yisingle/study/map/one/MainActivity.java
+++ app/src/main/java/com/yisingle/study/map/one/MainActivity.java
@@ -1,8 +1,11 @@
 package com.yisingle.study.map.one;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -24,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getApikey();
         tvInfo = findViewById(R.id.tvInfo);
         routeSearch = new RouteSearch(this);
 
@@ -79,5 +83,17 @@ public class MainActivity extends AppCompatActivity {
     public void toNavi(View view) {
         Intent intent = new Intent(this, NaviActivity.class);
         startActivity(intent);
+    }
+
+    public void getApikey() {
+        ApplicationInfo applicationInfo = null;
+        try {
+            applicationInfo = getPackageManager().getApplicationInfo(getPackageName(), PackageManager.GET_META_DATA);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (applicationInfo == null) return;
+        String value = applicationInfo.metaData.getString("com.amap.api.v2.apikey");
+        Log.e("测试代码", "测试代码Apikey="+value);
     }
 }
